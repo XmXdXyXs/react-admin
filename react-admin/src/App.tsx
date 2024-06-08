71a684202420430a6278f6a0c14e4c6cce78742d
XmXdXyXs <syc19950702@gmail.com>
2024-06-08 10:22:39 +0800

    feat: 'useContext createContext 使用'

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,31 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createContext, useState, useContext } from 'react'
 import './App.css'
-
+const NameContext: any = createContext({ name: null })
 function App() {
-	const [count, setCount] = useState(0)
-
+	const [name, setName] = useState('jay')
+	const handleName = () => {
+		setName('jay sun')
+	}
 	return (
 		<>
-			<div>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+			<NameContext.Provider value={{ name }}>
+				<p>学习react18</p>
+				<Child />
+				<button onClick={handleName}>名字</button>
+			</NameContext.Provider>
 		</>
 	)
+}
+interface User {
+	name: string | null
+}
+const Child = () => {
+	const user: User = useContext(NameContext)
+	return <div>{user.name}</div>
 }
 
 export default App
