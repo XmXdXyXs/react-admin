fa1cf36ffa848a718901625a9de0bcfdf609c7d0
XmXdXyXs <syc19950702@gmail.com>
2024-06-08 09:21:37 +0800

    feat: 'useMemo memo useCallback 使用'

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,31 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useMemo, useState, memo, useCallback } from 'react'
 import './App.css'
 
 function App() {
 	const [count, setCount] = useState(0)
-
+	// 每次有属性发生变化 total都会重新执行一次
+	const total = () => {
+		console.log('total 1')
+		const list = [1, 2, 3]
+		return list.reduce((prev, current) => {
+			return prev + current
+		}, 0)
+	}
+	// useMemo 缓存值 除非第二个参数的依赖发生改变 才会重新执行
+	const total2 = useMemo(() => {
+		console.log('total 2')
+		const list = [1, 2, 3]
+		return list.reduce((prev, current) => {
+			return prev + current
+		}, 0)
+	}, [])
+	// useCallback 缓存函数 除非第二个参数的依赖发生改变 才会重新执行
+	const handleChild = useCallback(() => {
+		console.log('子组件点击事件')
+	}, [])
+	const handleCount = () => {
+		setCount(count => count + 1)
+	}
 	return (
 		<>
-			<div>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+			<p>react18 学习</p>
+			<p>total1:{total()}</p>
+			<p>total2:{total2}</p>
+			<p>count:{count}</p>
+			<Child handleChild={handleChild} />
+			<button onClick={handleCount}>点击</button>
 		</>
 	)
 }
-
+interface Props {
+	handleChild: any
+}
+const Child = memo((props: Props) => {
+	console.log('子组件渲染')
+	return (
+		<>
+			<p onClick={props.handleChild}>子组件Child</p>
+		</>
+	)
+})
 export default App
