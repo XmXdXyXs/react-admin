013596af2a8e44ff978890e78180b74b65dd5b2a
XmXdXyXs <syc19950702@gmail.com>
2024-06-09 15:57:13 +0800

    feat: '实现子路由和动态路由'

--- react-admin/src/router2.tsx
+++ react-admin/src/router2.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Link, Navigate } from 'react-router-dom'
+import { createBrowserRouter, Link, Navigate, Outlet, useParams } from 'react-router-dom'
 import App from './App.tsx'
 
 export function RouterComponentReact() {
@@ -18,30 +18,84 @@ export function RouterComponentTest() {
 	)
 }
 
+export function RouterComponentShop() {
+	const params = useParams()
+	return (
+		<div>
+			<h1>商品商城</h1>
+			<p>商品id: {params.id}</p>
+			<p>订单id: {params.orderId}</p>
+		</div>
+	)
+}
+
+export function RouterComponentGoods() {
+	return (
+		<div>
+			<h1>商品主页</h1>
+			<Outlet></Outlet>
+		</div>
+	)
+}
+
 export function RouterComponentFound() {
 	return <div>404</div>
 }
-const routes = createBrowserRouter([
+const routes = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <App />
+		},
+		{
+			path: '/react',
+			element: <RouterComponentReact />
+		},
+		{
+			path: '/vite',
+			element: <RouterComponentVite />
+		},
+		{
+			path: '/test',
+			element: <RouterComponentTest />
+		},
+		{
+			path: '/shop/:id/order/:orderId',
+			element: <RouterComponentShop />
+		},
+		{
+			path: '/goods',
+			element: <RouterComponentGoods />,
+			children: [
+				{
+					path: 'list',
+					element: (
+						<div>
+							<p>苹果手机</p>
+							<p>华为手机</p>
+						</div>
+					)
+				},
+				{
+					path: 'cat',
+					element: (
+						<div>
+							<p>小米su7</p>
+							<p>极氪007</p>
+						</div>
+					)
+				}
+			]
+		},
+		{
+			path: '*',
+			element: <RouterComponentFound />
+		}
+	],
+	// 设置基础路由地址
 	{
-		path: '/',
-		element: <App />
-	},
-	{
-		path: '/react',
-		element: <RouterComponentReact />
-	},
-	{
-		path: '/vite',
-		element: <RouterComponentVite />
-	},
-	{
-		path: '/test',
-		element: <RouterComponentTest />
-	},
-	{
-		path: '*',
-		element: <RouterComponentFound />
+		basename: '/app'
 	}
-])
+)
 
 export default routes
